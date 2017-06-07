949c10c3594370f8c429be93a1916bb641b29181
Franky Liang <fliangz96@gmail.com>
2017-06-07 18:04:31 -0400

    WIP - stubbing methods

--- spec/lib/basic-raft/heartbeat_spec.rb
+++ spec/lib/basic-raft/heartbeat_spec.rb
@@ -10,28 +10,38 @@ describe "timer" do
   let!(:followers) { [f1,f2,f3 ] }
   let!(:leader) { subject }
 
+
+  # STUB A METHOD! Override is like we did for New!
+
   context "follower timeout" do
     # start election
     before { f1.start_timer }
+    before do
+      allow(f1).to receive(:node_timeout)
+    end
     it "starts an election on timeout" do
       # random between .15 and .3?
       # how to use start_timer here?
       sleep 1
       expect(f1).to have_received(:node_timeout)
-      expect(f1).to receive(:node_timeout)
-      expect(STDOUT).to receive(:puts).with('Start new election')
+      # expect(f1).to receive(:node_timeout)
+      # expect(STDOUT).to receive(:puts).with('Start new election')
     end
   end
 
   context "leader heartbeat" do
     before { subject.heartbeat }
+    before do
+      allow(f1).to receive(:append_entry)
+      allow(f1).to receive(:reset_timer)
+    end
     # maintain authority
     # should heartbeat happen endlessly?
     it "resets followers timeout" do
       # expect followers to reset timeout, so receive reset timer request
       followers.each do |f|
-        expect(f).to receive(:append_entry)
-        expect(f).to receive(:reset_timer)
+        expect(f1).to have_received(:append_entry)
+        expect(f1).to have_received(:reset_timer)
       end
     end
   end
