20cca2b2f5abd355579ed660343325d6780b7c78
Franky Liang <fliangz96@gmail.com>
2017-06-07 15:07:42 -0400

    Outline specs of heartbeat

--- spec/lib/basic-raft/heartbeat_spec.rb
+++ spec/lib/basic-raft/heartbeat_spec.rb
@@ -1,13 +1,27 @@
 describe "timer" do
-  context "follower timeout" do
+  include RSpec::EM::FakeClock
 
+  before { clock.stub }
+  after { clock.reset }
+
+  context "follower timeout" do
+    # start election
+    it "starts an election on timeout" do
+
+    end
   end
 
   context "leader heartbeat" do
+    # maintain authority
+    it "resets followers timeout" do
 
+    end
   end
 
-  context "election timeout" do
-    
+  context "candidate timeout" do
+    # split vote
+    it "starts a new election on timeout" do
+      
+    end
   end
 end
