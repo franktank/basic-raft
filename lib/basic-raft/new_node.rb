6d706641b208d91213eef10339ffa60356ebaf20
Franky Liang <fliangz96@gmail.com>
2017-06-08 13:03:53 -0400

    WIP - pre-parallel

--- lib/basic-raft/new_node.rb
+++ lib/basic-raft/new_node.rb
@@ -1,18 +1,22 @@
 require 'timers'
-
+require 'parallel'
 class NewNode
   def initialize(leader = nil)
     @current_timer = Timers::Group.new
     @cluster = []
     @log = []
+    @heartbeat_threads = []
+    @timer_thread
     if leader
       @role = :follower
       leader.handle_new_cluster_member(self)
       start_timer
+      p "TIMER"
     else
       @role = :leader
       add_to_cluster(self)
       heartbeat
+      p "HEARTBEAT"
     end
 
     # constantly checking to see if its a leader?
@@ -106,6 +110,7 @@ class NewNode
     else
       # Reset timer using parallel?
       reset_timer
+      p "RESET TIMER CALLED"
     end
   end
 
@@ -115,16 +120,28 @@ class NewNode
 
   def heartbeat
     # Just calls append_entry
-    Thread.new do
+    p "HEARTBEAT CALLED"
+    @heartbeat_threads << Thread.new do
       while true
         followers = get_followers
         followers.each do |f|
-          f = Thread.new do
+          @heartbeat_threads << Thread.new do
             f.append_entry
+            p "APPEND ENTRY CALLED"
           end
         end
       end
     end
+  end
+
+  def kill_heartbeat
+    # End heartbeat by ending the loop
+    p @heartbeat_threads
+    @heartbeat_threads.each do |hbt|
+      hbt.exit
+      p "THREAD TERMINATED"
+    end
+    # End all f threads created too
   end
 
   def follower_timeout
@@ -143,7 +160,7 @@ class NewNode
     elsif @role == :candidate
       p "Start new election"
     elsif @role == :follower
-      p "Start new election"
+       "Start new election"
     else
       p "Unknown role"
     end
@@ -155,18 +172,22 @@ class NewNode
   end
 
   def start_timer
-    new_timer = Timers::Group.new
-    @current_timer = new_timer
-    Thread.new do
+    # new_timer = Timers::Group.new
+    # @current_timer = new_timer
+    @timer_thread = Thread.new do
+      new_timer = Timers::Group.new
+      @current_timer = new_timer
       rnd = Random.new
       rnd_time = rnd.rand((15/100)..(30/100))
       p1 = new_timer.after(rnd_time) { node_timeout }
       @current_timer.wait
+      p "TIMEOUT TRIGGERED"
     end
   end
 
   def stop_timer
     @current_timer.cancel
+    @timer_thread.exit
   end
 
   ###################
--- spec/lib/basic-raft/heartbeat_spec.rb
+++ spec/lib/basic-raft/heartbeat_spec.rb
@@ -4,23 +4,23 @@ require_relative "../../../lib/basic-raft/new_node"
 describe "timer" do
 
   subject { NewNode.new }
-  let!(:f1) { NewNode.new(subject) }
-  let!(:f2) { NewNode.new(subject) }
-  let!(:f3) { NewNode.new(subject) }
-  let!(:clust) { [subject, f1, f2, f3] }
-  let!(:followers) { [f1,f2,f3 ] }
-  let!(:leader) { subject }
+  let(:f1) { NewNode.new(subject) }
+  let(:f2) { NewNode.new(subject) }
+  let(:f3) { NewNode.new(subject) }
+  let(:clust) { [subject, f1, f2, f3] }
+  let(:followers) { [f1,f2,f3 ] }
+  let(:leader) { subject }
 
 
   # STUB A METHOD! Override is like we did for New!
 
   context "follower timeout" do
-    # start election
-    before { f1.start_timer }
+    # before { subject.kill_heartbeat }
     before do
       allow(f1).to receive(:node_timeout)
     end
     it "starts an election on timeout" do
+      subject.kill_heartbeat
       # HOW DO I END THE HEARTBEATS ??????? :(
       # Leader is alive and sending heartbeats so test fails?
       expect(f1).to have_received(:node_timeout)
@@ -29,29 +29,43 @@ describe "timer" do
     end
   end
 
-  context "leader heartbeat" do
-    # Does the thread interfere with the test?
-    # How do threads work with RSpec?
-    # Printing in heartbeat appears in terminal
+  # context "leader heartbeat" do
+  #   # Does the thread interfere with the test?
+  #   # How do threads work with RSpec?
+  #   # Printing in heartbeat appears in terminal
+  #
+  #   before { subject.heartbeat }
+  #   before do
+  #     followers.each do |f|
+  #       allow(f).to receive(:append_entry)
+  #     end
+  #   end
 
-    before { subject.heartbeat }
-    before do
-      followers.each do |f|
-        allow(f).to receive(:append_entry)
-      end
-    end
-
-    it "resets followers timeout" do
-      followers.each do |f|
-        expect(f).to have_received(:append_entry)
-      end
-    end
-  end
-
-  context "candidate timeout" do
-    # split vote
-    it "starts a new election on timeout" do
-      pending "candidate not implemented yet"
-    end
-  end
+  #   it "is constantly beating if cluster contains a single leader" do
+  #
+  #   end
+  #
+  #   it "resets followers timeout" do
+  #     followers.each do |f|
+  #       expect(f).to have_received(:append_entry)
+  #     end
+  #   end
+  # end
+  #
+  # context "leader and follower timeout" do
+  #   it "triggers timeout when cluster is a leader and a follower" do
+  #
+  #   end
+  #
+  #   it "a follower receives timeout when cluster is a leader and 3 followers" do
+  #
+  #   end
+  # end
+  #
+  # context "candidate timeout" do
+  #   # split vote
+  #   it "starts a new election on timeout" do
+  #     pending "candidate not implemented yet"
+  #   end
+  # end
 end
